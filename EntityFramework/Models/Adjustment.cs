fe3c4197fc88e420aa02b845bd95e1f5c05f73c8
Charlie Ford <charles.ford1978@gmail.com>
2021-10-05 16:08:17 -0400

    Add project files.

---
+++ EntityFramework/Models/Adjustment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityFramework.Models
+{
+    public partial class Adjustment
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Gpadelta { get; set; }
+        public bool Completed { get; set; }
+    }
+}
